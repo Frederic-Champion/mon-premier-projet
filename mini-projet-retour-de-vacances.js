bce2b503dedd555646bf23152c30aa2eaabbddec
Fred <fredichampix@gmail.com>
2026-06-02 18:48:45 +0200

    retour de vacances

---
+++ mini-projet-retour-de-vacances.js
@@ -0,0 +1,9 @@
+const montures = [
+  { marque: "Ray-Ban",  modele: "Wayfarer", prix: 150, stock: 4,  categorie: "solaire", mutuelle: false },
+  { marque: "Persol",   modele: "649",      prix: 220, stock: 0,  categorie: "solaire", mutuelle: false },
+  { marque: "Oakley",   modele: "Holbrook", prix: 130, stock: 7,  categorie: "solaire", mutuelle: false },
+  { marque: "Essilor",  modele: "Varilux",  prix: 90,  stock: 12, categorie: "optique", mutuelle: true  },
+  { marque: "Krys",     modele: "Classic",  prix: 60,  stock: 3,  categorie: "optique", mutuelle: true  },
+  { marque: "Afflelou", modele: "Tonic",    prix: 75,  stock: 0,  categorie: "optique", mutuelle: true  },
+];
+
